f28badaf7f1694e625f70279fd09089c550297c3
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-10-12 22:58:34 +0900

    * added bunch of skeletons

---
+++ runwrf/conf.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+'''
+
+'''
+
+AUTHOR = "Yagnesh Raghava Yakkala"
+WEBSITE = "http://yagnesh.org"
+LICENSE ="GPL v3 or later"
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ runwrf/wrf.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+
+'''
+
+AUTHOR = "Yagnesh Raghava Yakkala"
+WEBSITE = "http://yagnesh.org"
+LICENSE ="GPL v3 or later"
+
+
+
+
+
+if __name__ == '__main__':
+    main()
