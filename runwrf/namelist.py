662622caf006cae4273346dd39425d732ec1edb0
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-10-13 17:01:38 +0900

    * runwrf/namelist.py: NameList class started

--- runwrf/namelist.py
+++ runwrf/namelist.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 '''
+  Read and manipulate the Fortran namelist files
+
+  Fotran namelist files are in the following pattern.
+
+________________________________________________________________
+     &section1
+        par = val
+        par2 = val,val2
+        ...
+     /
+
+     &section2
+        par = val
+        par4 = val,val2
+        ...
+     /
+________________________________________________________________
 
 '''
 
@@ -7,8 +24,27 @@ AUTHOR = "Yagnesh Raghava Yakkala"
 WEBSITE = "http://yagnesh.org"
 LICENSE ="GPL v3 or later"
 
+class NameList(dict):
+    """Read and Keep the Fortran namelist files.
+    """
+
+    def __init__(self, namelistfile):
+        """
+
+        Arguments:
+        - `namelistfile`:
+        """
+        dict.__init__(self)
+        self._namelistfile = namelistfile
+        self._fileContent = open(namelistfile).read()
+        self.
+
+    def print_raw_content(self):
+        return self._fileContent
 
 
+def main():
+    pass
 
 
 if __name__ == '__main__':
