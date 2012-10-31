de6efa86edaab71e4319129909d07fd0c487f1d8
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-10-31 23:15:27 +0900

    * setup.py: setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+import runwrf
+
+package = 'runwrf'
+version = '0.1'
+
+setup(
+    name='runwrf', version=runwrf.VERSION,
+    description="run WRF model with out any crud",
+    packages=[
+        'runwrf',
+        'runwrf.namelist',
+        'runwrf.tests',
+    ],
+    author=runwrf.AUTHOR,
+    author_email='hi@yagnesh.org',
+    license=runwrf.LICENSE,
+    url='http://github.com/yyr/runwrf',
+    keywords='WRF, weather Research, Model'
+    long_description = runwrf.__doc__,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python',
+    ],
+)
