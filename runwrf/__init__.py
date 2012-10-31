c1c26972d95cfe532f99c8e01e3eba43b60f9c9d
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-10-31 23:16:22 +0900

    * runwrf/__init__.py:  package

---
+++ runwrf/__init__.py
@@ -0,0 +1,24 @@
+'''
+Run WRF Model with out model any crud.
+Automate data downloading, running WPS, manipulating namelist, real and more.
+
+* `
+
+Install
+-------
+
+Usage
+-----
+
+
+
+'''
+
+DATE = "Sunday, May 13 2012"
+AUTHOR = "Yagnesh Raghava Yakkala"
+WEBSITE = "http://yagnesh.org"
+LICENSE ="GPL v3 or later"
+
+
+if __name__ == '__main__':
+    main()
